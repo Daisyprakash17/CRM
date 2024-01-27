b8282d0d1a9d12710450397b4984e0e65048c73b
Daisyprakash17 <daisyprakash007@gmail.com>
2024-01-27 19:10:27 +0530

    intial frontend routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import AuthRouter from './router/AuthRouter';
+import AppRouter from './router/AppRouter';
 
 function App() {
+  // const { isLoggedIn } = useSelector(selectAuth);
+  const isLoggedIn=true;
+
+  if (!isLoggedIn)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Localization>
+      <AuthRouter />
+    // </Localization>
   );
+else {
+  return(
+   // <Localization>
+   // <AppContextProvider>
+    //  <Suspense fallback={<PageLoader />}>
+        <AppRouter />
+   //   </Suspense>
+   // </AppContextProvider>
+  //</Localization>
+  )
+  
+}
+
 }
 
 export default App;
---
+++ frontend/src/router/AppRouter.js
@@ -0,0 +1,188 @@
+import { lazy } from 'react';
+
+import { useRoutes } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+
+export default function AppRouter() {
+  let element = useRoutes([
+    {
+      path: '/login',
+      element: <Navigate to="/" />,
+    },
+    {
+      path: '/verify/*',
+      element: <Navigate to="/" />,
+    },
+    {
+      path: '/resetpassword/*',
+      element: <Navigate to="/" />,
+    },
+    {
+      path: '/logout',
+      element: <Logout />,
+    },
+    {
+      path: '/about',
+      element: <About />,
+    },
+    {
+      path: '/',
+      element: <Dashboard />,
+    },
+    {
+      path: '/customer',
+      element: <Customer />,
+    },
+    {
+      path: '/people',
+      element: <People />,
+    },
+    {
+      path: '/company',
+      element: <Company />,
+    },
+    {
+      path: '/expenses',
+      element: <Expense />,
+    },
+    {
+      path: '/product',
+      element: <Product />,
+    },
+    {
+      path: '/category/product',
+      element: <ProductCategory />,
+    },
+    {
+      path: 'category/expenses',
+      element: <ExpenseCategory />,
+    },
+    {
+      path: '/inventory',
+      element: <Inventory />,
+    },
+    {
+      path: '/order',
+      element: <Order />,
+    },
+    {
+      path: '/invoice',
+      element: <Invoice />,
+    },
+    {
+      path: '/invoice/create',
+      element: <InvoiceCreate />,
+    },
+    {
+      path: '/invoice/read/:id',
+      element: <InvoiceRead />,
+    },
+    {
+      path: '/invoice/update/:id',
+      element: <InvoiceUpdate />,
+    },
+    {
+      path: '/invoice/pay/:id',
+      element: <InvoiceRecordPayment />,
+    },
+    {
+      path: '/quote',
+      element: <Quote />,
+    },
+    {
+      path: '/quote/create',
+      element: <QuoteCreate />,
+    },
+    {
+      path: '/quote/read/:id',
+      element: <QuoteRead />,
+    },
+    {
+      path: '/quote/update/:id',
+      element: <QuoteUpdate />,
+    },
+    {
+      path: '/payment',
+      element: <Payment />,
+    },
+    {
+      path: '/payment/read/:id',
+      element: <PaymentRead />,
+    },
+    {
+      path: '/payment/update/:id',
+      element: <PaymentUpdate />,
+    },
+    {
+      path: '/employee',
+      element: <Employee />,
+    },
+    {
+      path: '/admin',
+      element: <Admin />,
+    },
+    {
+      path: '/settings',
+      element: <Settings />,
+    },
+    {
+      path: '/settings/edit/:settingsKey',
+      element: <Settings />,
+    },
+    {
+      path: '/payment/mode',
+      element: <PaymentMode />,
+    },
+    {
+      path: '/taxes',
+      element: <Taxes />,
+    },
+    {
+      path: '/email',
+      element: <Email />,
+    },
+    {
+      path: '/email/read/:id',
+      element: <EmailRead />,
+    },
+    {
+      path: '/email/update/:id',
+      element: <EmailUpdate />,
+    },
+
+    {
+      path: '/settings/advanced',
+      element: <AdvancedSettings />,
+    },
+    {
+      path: '/profile',
+      element: <Profile />,
+    },
+    {
+      path: '/lead',
+      element: <Lead />,
+    },
+    {
+      path: '/offer',
+      element: <Offer />,
+    },
+    {
+      path: '/offer/create',
+      element: <OfferCreate />,
+    },
+    {
+      path: '/offer/read/:id',
+      element: <OfferRead />,
+    },
+    {
+      path: '/offer/update/:id',
+      element: <OfferUpdate />,
+    },
+    {
+      path: '*',
+      element: <NotFound />,
+    },
+  ]);
+
+  return element;
+}
