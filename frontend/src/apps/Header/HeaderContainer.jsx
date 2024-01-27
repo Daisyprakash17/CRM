bc96041a83f0160139f11e06c83dfdea37ee4df8
Daisyprakash17 <daisyprakash007@gmail.com>
2024-01-27 22:46:14 +0530

    header and left panel

---
+++ frontend/src/apps/Header/HeaderContainer.jsx
@@ -0,0 +1,161 @@
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Avatar, Dropdown, Layout } from 'antd';
+
+// import Notifications from '@/components/Notification';
+
+import { SettingOutlined, LogoutOutlined } from '@ant-design/icons';
+
+// import { checkImage } from '@/request';
+
+// import { selectCurrentAdmin } from '@/redux/auth/selectors';
+
+import { useNavigate } from 'react-router-dom';
+
+// import { BASE_URL } from '@/config/serverApiConfig';
+const BASE_URL="bigTech.com";
+
+// import useLanguage from '@/locale/useLanguage';
+
+
+
+// import SelectLanguage from '@/components/SelectLanguage';
+
+// import UpgradeButton from './UpgradeButton';
+
+export default function HeaderContent() {
+  // const currentAdmin = useSelector(selectCurrentAdmin);
+  const currentAdmin={name:"dasiy",surname:"khoja",email:"daisykhoja@.com",photo:"",}
+  const { Header } = Layout;
+
+  // const translate = useLanguage();
+
+
+  const [hasPhotoprofile, setHasPhotoprofile] = useState(false);
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     if (currentAdmin?.photo) {
+  //       const result = await checkImage(BASE_URL + currentAdmin?.photo);
+  //       setHasPhotoprofile(result);
+  //     }
+  //   }
+  //   fetchData();
+  //   return () => {
+  //     return false;
+  //   };
+  // }, []);
+
+  const ProfileDropdown = () => {
+    const navigate = useNavigate();
+    return (
+      <div className="profileDropdown" onClick={() => navigate('/profile')}>
+        <Avatar
+          size="large"
+          className="last"
+          // src={hasPhotoprofile ? BASE_URL + currentAdmin?.photo : null}
+          src={"https://rb.gy/8eri8x"}
+          style={{ color: '#f56a00', backgroundColor: !hasPhotoprofile ? '#fde3cf' : '#f9fafc' }}
+        >
+          {currentAdmin?.name?.charAt(0)?.toUpperCase()}
+        </Avatar>
+        <div className="profileDropdownInfo">
+          <p>
+            {currentAdmin?.name} {currentAdmin?.surname}
+          </p>
+          <p>{currentAdmin?.email}</p>
+        </div>
+      </div>
+    );
+  };
+
+  const DropdownMenu = ({ text }) => {
+    return <span style={{}}>{text}</span>;
+  };
+
+  const items = [
+    {
+      label: <ProfileDropdown className="headerDropDownMenu" />,
+      key: 'ProfileDropdown',
+    },
+    {
+      type: 'divider',
+    },
+    {
+      icon: <SettingOutlined />,
+      key: 'settingProfile',
+      label: (
+        <Link to={'/profile'}>
+          {/* <DropdownMenu text={translate('profile_settings')} /> */}
+          <DropdownMenu text={"Profile settings"} />
+        </Link>
+      ),
+    },
+    {
+      icon: <SettingOutlined />,
+      key: 'settingApp',
+      // label: <Link to={'/settings'}>{translate('app_settings')}</Link>,
+      label: <Link to={'/settings'}>{"App settings"}</Link>,
+    },
+
+    {
+      type: 'divider',
+    },
+
+    {
+      icon: <LogoutOutlined />,
+      key: 'logout',
+      // label: <Link to={'/logout'}>{translate('logout')}</Link>,
+      label: <Link to={'/logout'}>{"Logout"}</Link>,
+    },
+  ];
+  return (
+    <Header
+      style={{
+        padding: '20px',
+        background: '#f9fafc',
+        display: ' flex',
+        flexDirection: ' row-reverse',
+        justifyContent: ' flex-start',
+        gap: ' 15px',
+      }}
+    >
+      <Dropdown
+        menu={{
+          items,
+        }}
+        trigger={['click']}
+        placement="bottomRight"
+        stye={{ width: '280px', float: 'right' }}
+      >
+        {/* <Badge dot> */}
+        <Avatar
+          className="last"
+          // src={hasPhotoprofile ? BASE_URL + currentAdmin?.photo : null}
+          src={"https://rb.gy/8eri8x"}
+          style={{
+            color: '#f56a00',
+            backgroundColor: !hasPhotoprofile ? '#fde3cf' : '#f9fafc',
+            float: 'right',
+            cursor: 'pointer',
+          }}
+          size="large"
+        >
+          {currentAdmin?.name?.charAt(0)?.toUpperCase()}
+        </Avatar>
+        {/* </Badge> */}
+      </Dropdown>
+
+      {/* <UpgradeButton /> */}
+      UPGRADE BUTTON
+
+      {/* <SelectLanguage /> */}
+      SELECT LANGUAGE
+    </Header>
+  );
+}
+
+//  console.log(
+//    '🚀 Welcome to IDURAR ERP CRM! Did you know that we also offer commercial customization services? Contact us at hello@idurarapp.com for more information.'
+//  );
