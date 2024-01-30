54a49a80d3794a445f47331754bb26d720fe93bc
vijayan-0908 <ujjvalm_chouhan@yahoo.com>
2024-01-30 21:44:41 +0530

    some frontend forms

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+// import NotFound from '@/components/NotFound';
+import NotFound from '../components/NotFound';
+
+const NotFoundPage = () => {
+  return <NotFound entity={''} />;
+};
+export default NotFoundPage;
