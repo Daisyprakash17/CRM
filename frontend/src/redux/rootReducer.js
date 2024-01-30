75c49fcbf98a4eb4d7a81b36c42212e642bba9ab
Daisyprakash17 <daisyprakash007@gmail.com>
2024-01-30 21:46:10 +0530

    auth reducer setup

--- frontend/src/redux/rootReducer.js
+++ frontend/src/redux/rootReducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
 
 import {reducer as translateReducer} from "./translate"
-
+import {reducer as authReducer} from './auth'
 
 const rootReducer=combineReducers({
     translate:translateReducer,
+    auth:authReducer
 })
 
 export default rootReducer;
